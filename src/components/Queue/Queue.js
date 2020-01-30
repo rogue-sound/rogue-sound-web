bf9e94ba8e7d5dbb58518c37201371f5293e08a8
cesarandex <cexposif@everis.com>
2020-01-30 01:05:59 +0100

    Increased mockup and added styles

--- src/components/Queue/Queue.js
+++ src/components/Queue/Queue.js
@@ -22,6 +22,46 @@ const mockedItems = [
     artist: 'Tame Impala',
     user: 'bonavida',
   },
+  {
+    albumName: 'Album',
+    albumImg:
+      'https://images.unsplash.com/photo-1504083898675-c896ecdae86e?fm=jpg&w=1080&fit=max&ixid=eyJhcHBfaWQiOjYzNDZ9',
+    title: 'Anaconda',
+    artist: 'Nicki Minaj',
+    user: 'mateoberno',
+  },
+  {
+    albumName: 'Album',
+    albumImg:
+      'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?fm=jpg&w=1080&fit=max&ixid=eyJhcHBfaWQiOjYzNDZ9',
+    title: 'Sin Pijama',
+    artist: 'Becky G',
+    user: 'tigresadeloriente',
+  },
+  {
+    albumName: 'Album',
+    albumImg:
+      'https://images.unsplash.com/photo-1549007247-d4399d43df67?fm=jpg&w=1080&fit=max&ixid=eyJhcHBfaWQiOjYzNDZ9',
+    title: 'Calocha',
+    artist: 'Barbie Rican ft. Jamsha',
+    user: 'protein_as',
+  },
+  {
+    albumName: 'Album',
+    albumImg:
+      'https://images.unsplash.com/photo-1541164757216-e5c2c2b5d69b?fm=jpg&w=1080&fit=max&ixid=eyJhcHBfaWQiOjYzNDZ9',
+    title: 'Girlfriend (Spanish Version)',
+    artist: 'Avril Lavigne',
+    user: 'avrilTheQueen',
+  },
+  {
+    albumName: 'Album',
+    albumImg:
+      'https://images.unsplash.com/photo-1527500335527-cdb637d6d605?fm=jpg&w=1080&fit=max&ixid=eyJhcHBfaWQiOjYzNDZ9',
+    title: 'Gasolina',
+    artist: 'Daddy Yankee',
+    user: 'bonavida',
+  },
 ];
 
 const Queue = () => (
