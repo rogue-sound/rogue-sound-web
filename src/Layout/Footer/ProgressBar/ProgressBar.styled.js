9c2dda564edfa85cab076c84d8e47bc45e3b9040
Diego Bonavida <gecko.bona@gmail.com>
2020-02-23 15:11:43 +0100

    Width is 0 when percentage is undefined - Deleted progress bar knob (not used ATM)

--- src/Layout/Footer/ProgressBar/ProgressBar.styled.js
+++ src/Layout/Footer/ProgressBar/ProgressBar.styled.js
@@ -9,23 +9,18 @@ const ProgressBarContainer = styled.div`
   z-index: 3;
 `;
 
-const ProgressBarPlaying = styled.div`
+const ProgressBarPlaying = styled.div.attrs(props => ({
+  style: {
+    width: `${
+      props.percentage ? (props.percentage > 100 ? 100 : props.percentage) : 0
+    }%`,
+  },
+}))`
   height: 10px;
-  width: ${props => (props.percentage > 100 ? 100 : props.percentage)}%;
   background: linear-gradient(to right, #835fc1, #01ab6d);
   display: flex;
   align-items: center;
   cursor: pointer;
 `;
 
-const ProgressBarKnob = styled.span`
-  position: relative;
-  height: 16px;
-  width: 16px;
-  left: calc(${props => props.percentage}% - 9px);
-  border: 1.5px solid white;
-  border-radius: 50%;
-  background-color: #01ab6d;
-`;
-
-export { ProgressBarContainer, ProgressBarPlaying, ProgressBarKnob };
+export { ProgressBarContainer, ProgressBarPlaying };
