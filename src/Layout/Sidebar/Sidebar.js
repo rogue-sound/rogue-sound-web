7fc6a1ce7cbe0f6f81c529115d3955b8b3cd0a63
mateobemo <mateobernal19@gmail.com>
2020-03-05 22:05:43 +0100

    translation fix

--- src/Layout/Sidebar/Sidebar.js
+++ src/Layout/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 /** Components */
 import Tabs from '@common/Tabs';
 import Queue from './Queue';
@@ -6,16 +7,24 @@ import Queue from './Queue';
 /** Styled components */
 import { SidebarWrapper } from './sidebar.styled';
 
-const Sidebar = () => (
-  <SidebarWrapper>
-    <Tabs>
-      <Queue label="Queue" icon="align-justify" />
-      {/* <div label="Chat" icon="comment-dots">
+const Sidebar = () => {
+  const intl = useIntl();
+  return (
+    <SidebarWrapper>
+      <Tabs>
+        <Queue
+          label={intl.formatMessage({
+            id: 'app.layout.Sidebar.Queue.QueueText',
+          })}
+          icon="align-justify"
+        />
+        {/* <div label="Chat" icon="comment-dots">
         After while, <em>Crocodile</em>!
       </div> */}
-    </Tabs>
-  </SidebarWrapper>
-);
+      </Tabs>
+    </SidebarWrapper>
+  );
+};
 
 Sidebar.propTypes = {};
 
