4d4f25ee7d8be89f963d9bc8299b09b1b42a01be
cesarandex <cesarandex@gmail.com>
2021-01-22 18:08:46 +0100

    Apply new Github SPA workaround

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -51,25 +51,19 @@ export const retrieveSpotifyToken = () => {
 
 export const githubSPA = () => {
   const l = window.location;
-  if (l.search) {
-    const q = {};
-    l.search
+  if (l.search[1] === '/') {
+    const decoded = l.search
       .slice(1)
       .split('&')
-      .forEach(v => {
-        const a = v.split('=');
-        q[a[0]] = a
-          .slice(1)
-          .join('=')
-          .replace(/~and~/g, '&');
-      });
-    if (q.p !== undefined) {
-      window.history.replaceState(
-        null,
-        null,
-        l.pathname.slice(0, -1) + (q.p || '') + (q.q ? `?${q.q}` : '') + l.hash
-      );
-    }
+      .map(s => {
+        return s.replace(/~and~/g, '&');
+      })
+      .join('?');
+    window.history.replaceState(
+      null,
+      null,
+      l.pathname.slice(0, -1) + decoded + l.hash
+    );
   }
 };
 
