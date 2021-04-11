aae2003529f576d057be1c7a4e35857fbb5798e7
cesarandex <cesarandex@gmail.com>
2021-04-11 12:16:26 +0200

    Refactor play component

--- src/pages/Room/Play/Play.js
+++ src/pages/Room/Play/Play.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
@@ -13,104 +13,117 @@ import './Play.scss';
 const Play = ({ room: { id: roomId, style: roomStyle } }) => {
   const intl = useIntl();
   const [remaining, setRemaining] = useState(null);
-  const remainingRef = useRef(remaining);
-  remainingRef.current = remaining;
-  const [joinTimeout, setJoinTimeout] = useState(null);
+  const [playTimeout, setPlayTimeout] = useState(null);
   const [pollingState, setPollingState] = useState(false);
-  const [disabledRepeat, setDisabledRepeat] = useState(false);
+  const dumbPolling = useRef(null);
+  const disabledRepeat = useRef(false);
 
   const reduxCurrent = useSelector(state => state.playing.current);
   const { devices, activeDevice } = useSelector(state => state.spotify);
 
   const dispatch = useDispatch();
 
-  const handleJoin = async (smart = false) => {
-    try {
-      if (!roomId || !roomStyle) return;
-      const { current, songs } = await getCurrent(`${roomId}${roomStyle}`);
-      if (smart || (!remainingRef.current && current)) {
-        const song = {
-          uris: [current.songId],
-          position_ms: current.position,
+  /* Play song */
+  const _playSong = useCallback(
+    async (song, device) => {
+      // Review this condition to setCurrent on end
+      if (!device || song.songId === reduxCurrent.songId) return;
+      try {
+        const _song = {
+          uris: [song.songId],
+          position_ms: song.position,
         };
-        if (activeDevice) {
-          await playSong(song, activeDevice);
-          if (!disabledRepeat) {
-            disableRepeat(activeDevice);
-            setDisabledRepeat(true);
-          }
-          dispatch(setCurrent(current));
-          const remainingTime = current.duration - current.position;
-          setRemaining(remainingTime);
+        await playSong(_song, device);
+        if (!disabledRepeat.current) {
+          disableRepeat(device);
+          disabledRepeat.current = true;
         }
+        dispatch(setCurrent(song));
+      } catch {
+        dispatch(stop());
+        setRemaining(null);
       }
-      songs && dispatch(setQueue(songs));
-    } catch {
-      dispatch(stop());
-      setRemaining(null);
-      setJoinTimeout(null);
-    }
-  };
+    },
+    [dispatch, reduxCurrent]
+  );
 
-  useEffect(() => {
-    if (activeDevice) {
-      handleJoin(true);
-    } else {
-      handleJoin();
-    }
-  }, [activeDevice, roomId]);
+  /* Get room information (current song and queue) */
+  const getRoomInformation = useCallback(
+    async (updateCurrent = true, updateQueue = true) => {
+      if (!roomId || !roomStyle) return [];
+      const { current, songs } = await getCurrent(`${roomId}${roomStyle}`);
+      if (updateQueue && songs) dispatch(setQueue(songs));
+      if (updateCurrent && activeDevice) {
+        _playSong(current, activeDevice);
+      }
+      return [current, songs];
+    },
+    [roomId, roomStyle, _playSong, dispatch, activeDevice]
+  );
 
-  // Dumb polling
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      handleJoin();
-      setPollingState(!pollingState);
-    }, 4000);
-    return () => clearTimeout(timeout);
-  }, [pollingState]);
+  /* TODO: Fix rejoin when new device is selected */
+  // useEffect(() => {
+  //   console.log(`activeDevice`, activeDevice);
+  //   getRoomInformation();
+  // }, [activeDevice]);
 
-  // Smart polling
+  /* TODO: Join session before first dumb polling exeuction */
+
+  /* Set up smart polling */
   useEffect(() => {
+    if (!reduxCurrent) setRemaining(null);
+    console.log('Setting up smart polling');
+    const { duration, position } = reduxCurrent;
+    const remainingTime = duration - position;
+    setRemaining(remainingTime);
+  }, [reduxCurrent]);
+
+  /* Handle smart polling */
+  useEffect(() => {
+    playTimeout && clearTimeout(playTimeout);
     if (remaining) {
-      joinTimeout && clearTimeout(joinTimeout);
-      setJoinTimeout(setTimeout(() => handleJoin(true), remaining));
+      setPlayTimeout(
+        setTimeout(() => {
+          getRoomInformation();
+        }, remaining)
+      );
     }
-    return () => clearTimeout(joinTimeout);
-  }, [remaining]);
+
+    return () => {
+      console.log('[UNMOUNT] Clearing smart polling');
+      clearTimeout(playTimeout);
+    };
+  }, [remaining, getRoomInformation]);
+
+  /* Dumb polling */
+  useEffect(() => {
+    const { current: currentPolling } = dumbPolling;
+    if (currentPolling) clearTimeout(currentPolling);
+    dumbPolling.current = setTimeout(() => {
+      getRoomInformation();
+      setPollingState(prev => !prev);
+    }, 4000);
+
+    return () => {
+      console.log('[UNMOUNT] Clearing dumb polling');
+      clearTimeout(currentPolling);
+    };
+  }, [pollingState, getRoomInformation]);
+
+  const notificationMessage = id => (
+    <p className="notification-message">
+      {intl.formatMessage({ id: `app.pages.Room.Play.${id}` })}
+    </p>
+  );
 
   const renderPlay = () => {
-    if (!devices.length)
-      return (
-        <p className="no-available-devices">
-          {intl.formatMessage({
-            id: 'app.pages.Room.Play.NoAvailableDevicesText',
-          })}
-        </p>
-      );
-
-    if (!activeDevice)
-      return (
-        <p className="no-available-devices">
-          {intl.formatMessage({
-            id: 'app.pages.Room.Play.NoActiveDeviceText',
-          })}
-        </p>
-      );
-
-    if (!remaining) {
-      return (
-        <p className="no-current-song">
-          {intl.formatMessage({
-            id: 'app.pages.Room.Play.SessionNotStartedText',
-          })}
-        </p>
-      );
-    }
+    if (!devices.length) return notificationMessage('NoAvailableDevicesText');
+    if (!activeDevice) return notificationMessage('NoActiveDeviceText');
+    if (!remaining) return notificationMessage('SessionNotStartedText');
 
     return <CurrentSong {...reduxCurrent} />;
   };
 
-  // TODO: Add admin buttons?
   return (
     <>
       {!!remaining && (
